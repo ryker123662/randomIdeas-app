d7c0fd24e87b0ca3a7276c7af29054e218748bc8
ryker123662 <adamtomasz96@gmail.com>
2024-06-11 18:17:22 +0200

    removed font npm package nad added a cdn one

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,3 @@
-import '@fortawesome/fontawesome-free/css/all.css';
 import Modal from './components/Modal';
 import IdeaForm from './components/IdeaForm';
 import IdeaList from './components/IdeaList';
